b1ca13ccd3cfe47cb591a778bbb92143915e486a
Gary Hake <Gary.Hake@waystar.com>
2021-01-02 10:53:27 -0500

    abstract classes

---
+++ 03-classes-interfaces/06-abstract-classes.ts
@@ -0,0 +1,95 @@
+namespace AbstractClass {
+	// abstract classes can't be instantiated
+	abstract class Department {
+		protected employees: string[] = [];
+
+		constructor(protected readonly id: string, public name: string) {}
+
+		// Abstract
+		abstract describe(this: Department): void;
+
+		addEmployee(employee: string) {
+			this.employees.push(employee);
+		}
+
+		printEmployeeInfo() {
+			console.log(this.employees.length);
+			console.log(this.employees);
+		}
+	}
+
+	class ITDepartment extends Department {
+		admins: string[];
+
+		constructor(id: string, admins: string[]) {
+			super(id, 'IT');
+			this.admins = admins; // this has to come after super
+		}
+
+		describe() {
+			console.log(`IT Department - ID: ${this.id}`);
+		}
+	}
+
+	class AccountingDepartment extends Department {
+		private lastReport: string;
+
+		get mostRecentReport() {
+			if (this.lastReport) {
+				return this.lastReport;
+			}
+			throw new Error('No report found.');
+		}
+
+		set mostRecentReport(value: string) {
+			if (!value) {
+				throw new Error('Please pass in a valid value');
+			}
+			this.addReport(value);
+		}
+
+		constructor(id: string, private reports: string[]) {
+			super(id, 'Accounting');
+
+			if (reports[0]) {
+				this.lastReport = reports[0];
+			} else {
+				this.lastReport = '';
+			}
+		}
+
+		addEmployee(name: string) {
+			if (name === 'Gary') {
+				return;
+			}
+			this.employees.push(name);
+		}
+
+		addReport(text: string) {
+			this.reports.push(text);
+			this.lastReport = text;
+		}
+
+		getReports() {
+			console.log(this.reports);
+		}
+
+		describe() {
+			console.log(`Accounting Department - ID: ${this.id}`);
+		}
+	}
+
+	const itDept = new ITDepartment('d2', ['Gary']);
+	itDept.addEmployee('Gary');
+	itDept.addEmployee('Rowe');
+
+	const accounting = new AccountingDepartment('d1', []);
+	accounting.addEmployee('Gary');
+	accounting.addEmployee('Rowe');
+
+	accounting.addReport('Something Went Wrong');
+	console.log(accounting.mostRecentReport);
+
+	accounting.mostRecentReport = 'New Report';
+	console.log(accounting.mostRecentReport);
+}
