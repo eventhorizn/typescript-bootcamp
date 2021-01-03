4af561edb0c81583dc147a31b6e456fbc44e4922
Gary Hake <Gary.Hake@waystar.com>
2021-01-02 20:22:50 -0500

    type guards

---
+++ 04-advanced-types/02-type-guards.ts
@@ -0,0 +1,83 @@
+namespace TypeGuards {
+	type Admin = {
+		name: string;
+		privileges: string[];
+	};
+
+	type Employee = {
+		name: string;
+		startDate: Date;
+	};
+
+	type ElevatedEmployee = Admin & Employee;
+
+	const el: ElevatedEmployee = {
+		name: 'Gary',
+		privileges: ['create-server'],
+		startDate: new Date(),
+	};
+
+	type Combinable = string | number;
+	type Numeric = number | boolean;
+	type Universal = Combinable & Numeric;
+
+	function add(a: Combinable, b: Combinable) {
+		// This is called a 'type guard'
+		if (typeof a === 'string' || typeof b === 'string') {
+			return a.toString() + b.toString();
+		}
+
+		return a + b;
+	}
+
+	type UnknownEmployee = Employee | Admin;
+
+	function printEmployeeInformation(emp: UnknownEmployee) {
+		console.log(`Name: ${emp.name}`);
+
+		// confirming if the property exists in object
+		if ('privileges' in emp) {
+			console.log(`Privileges: ${emp.privileges}`);
+		}
+
+		if ('startDate' in emp) {
+			console.log(`Start Date: ${emp.startDate}`);
+		}
+	}
+
+	printEmployeeInformation(el);
+
+	class Car {
+		drive() {
+			console.log('Driving...');
+		}
+	}
+
+	class Truck {
+		drive() {
+			console.log('Driving a truck...');
+		}
+
+		loadCargo(amount: number) {
+			console.log('Loading cargo... ' + amount);
+		}
+	}
+
+	type Vehicle = Car | Truck;
+
+	const v1 = new Car();
+	const v2 = new Truck();
+
+	function useVehicle(vehicle: Vehicle) {
+		vehicle.drive();
+
+		// now what about 'loadCargo'
+		// instanceof is great
+		if (vehicle instanceof Truck) {
+			vehicle.loadCargo(1000);
+		}
+	}
+
+	useVehicle(v1);
+	useVehicle(v2);
+}
