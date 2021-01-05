541bd826cd996f00fc7f3dfe5a23a87b4e736d12
Gary Hake <Gary.Hake@waystar.com>
2021-01-05 08:11:37 -0500

    adding classes to remove 'any' types

--- 07-drag-drop-project/src/app.ts
+++ 07-drag-drop-project/src/app.ts
@@ -1,22 +1,41 @@
+// Project Type
+enum ProjectStatus {
+	Active,
+	Finished,
+}
+
+class Project {
+	constructor(
+		public id: string,
+		public title: string,
+		public description: string,
+		public people: number,
+		public status: ProjectStatus
+	) {}
+}
+
 // Project State Management
+type Listener = (items: Project[]) => void; // function type
+
 class ProjectState {
-	private listeners: any[] = [];
-	private projects: any[] = [];
+	private listeners: Listener[] = [];
+	private projects: Project[] = [];
 	private static instance: ProjectState;
 
 	private constructor() {}
 
-	addListener(listenerFn: Function) {
+	addListener(listenerFn: Listener) {
 		this.listeners.push(listenerFn);
 	}
 
 	addProject(title: string, description: string, numPeople: number) {
-		const newProject = {
-			id: Math.random().toString(),
-			title: title,
-			description: description,
-			people: numPeople,
-		};
+		const newProject = new Project(
+			Math.random().toString(),
+			title,
+			description,
+			numPeople,
+			ProjectStatus.Active
+		);
 
 		this.projects.push(newProject);
 
@@ -106,7 +125,7 @@ class ProjectList {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLElement;
-	assignedProjects: any[];
+	assignedProjects: Project[];
 
 	constructor(private type: 'active' | 'finished') {
 		this.templateElement = document.getElementById(
@@ -124,7 +143,7 @@ class ProjectList {
 		this.element = importedNode.firstElementChild as HTMLElement;
 		this.element.id = `${this.type}-projects`;
 
-		projectState.addListener((projects: any[]) => {
+		projectState.addListener((projects: Project[]) => {
 			this.assignedProjects = projects;
 			this.renderProjects();
 		});
